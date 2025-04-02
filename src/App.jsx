36697a2a1055475b4b02ab3c866b4269a719b45e
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-04-02 23:27:51 +0300

    add index.js and app.js file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<ProductListingPage />} />
+          <Route path="/cart" element={<ShoppingCartPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
